9eb92bfad4e77aa67948cd198fd7c20134781fa0
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 01:52:44 +0530

    initial commit

---
+++ simple_hash/src/main.rs
@@ -0,0 +1,15 @@
+use sha2::{Sha256, Digest};
+
+fn hash_message(message: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(message.as_bytes());
+    let result = hasher.finalize();
+    format!("{:x}", result)
+}
+
+fn main() {
+    let message = "Hello, Rust Zero Knowledge!";
+    let hash = hash_message(message);
+    println!("Message: {}", message);
+    println!("SHA-256 Hash: {}", hash);
+}
