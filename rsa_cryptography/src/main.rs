a32f977decf11a89f924a2fd47d210d88da9698e
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 02:05:29 +0530

    initial commit

--- rsa_cryptography/src/main.rs
+++ rsa_cryptography/src/main.rs
@@ -6,4 +6,20 @@ fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
     let public_key = RsaPublicKey::from(&private_key);
     (private_key, public_key)
 }
+use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
+
+fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
+    let private_key = RsaPrivateKey::new(&mut OsRng, 2048).expect("Failed to generate a key");
+    let public_key = RsaPublicKey::from(&private_key);
+    (private_key, public_key)
+}
+
+fn encrypt_message(public_key: &RsaPublicKey, message: &str) -> Vec<u8> {
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+    public_key
+        .encrypt(&mut OsRng, padding, message.as_bytes())
+        .expect("Failed to encrypt")
+}
+
 
