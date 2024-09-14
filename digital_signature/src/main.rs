24cf9ebaf96237004ed7363a42fb78be2c7103fa
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 05:22:09 +0530

    initial commit

--- digital_signature/src/main.rs
+++ digital_signature/src/main.rs
@@ -19,3 +19,26 @@ fn sign_message(private_key: &RsaPrivateKey, message: &str) -> Vec<u8> {
         .expect("Failed to sign message")
 }
 
+fn verify_signature(public_key: &RsaPublicKey, message: &str, signature: &[u8]) -> bool {
+    let mut hasher = Sha256::new();
+    hasher.update(message.as_bytes());
+    let hashed_message = hasher.finalize();
+
+    let padding = PaddingScheme::new_pkcs1v15_sign(Some(rsa::pkcs1v15::Hash::SHA2_256));
+
+    public_key
+        .verify(padding, &hashed_message, signature)
+        .is_ok()
+}
+
+fn main() {
+    let (private_key, public_key) = generate_rsa_keypair();
+    let message = "Hello, Rust Zero Knowledge!";
+    println!("Original Message: {}", message);
+
+    let signature = sign_message(&private_key, message);
+    println!("Signature: {:?}", signature);
+
+    let is_valid = verify_signature(&public_key, message, &signature);
+    println!("Signature valid: {}", is_valid);
+}
