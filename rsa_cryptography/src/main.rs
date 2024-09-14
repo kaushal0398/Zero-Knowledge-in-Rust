4e398138234066ac701098d872ae4b40f3bface8
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 02:22:24 +0530

    Updated Code

--- rsa_cryptography/src/main.rs
+++ rsa_cryptography/src/main.rs
@@ -6,6 +6,14 @@ fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
     let public_key = RsaPublicKey::from(&private_key);
     (private_key, public_key)
 }
+use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
+
+fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
+    let private_key = RsaPrivateKey::new(&mut OsRng, 2048).expect("Failed to generate a key");
+    let public_key = RsaPublicKey::from(&private_key);
+    (private_key, public_key)
+}
 
 fn encrypt_message(public_key: &RsaPublicKey, message: &str) -> Vec<u8> {
     let padding = PaddingScheme::new_pkcs1v15_encrypt();
@@ -33,3 +41,4 @@ fn main() {
     let decrypted_message = decrypt_message(&private_key, &encrypted_message);
     println!("Decrypted Message: {}", decrypted_message);
 }
+
