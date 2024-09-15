c6395f8dd31759b29779743fd0fa4de0f7d8fc7a
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 12:47:35 +0530

    Updated

--- 7_zk_snark/src/main.rs
+++ 7_zk_snark/src/main.rs
@@ -1,1 +1,16 @@
+use bellman::{Circuit, ConstraintSystem, SynthesisError};
+use pairing::bn256::{Bn256, Fr};
+use bellman::groth16::{generate_random_parameters, create_random_proof, verify_proof, prepare_verifying_key};
+use rand::thread_rng;
+use bellman::gadgets::num::AllocatedNum;
 
+struct SimpleCircuit {
+    pub a: Option<Fr>,
+    pub b: Option<Fr>,
+    pub c: Option<Fr>,
+}
+
+impl Circuit<Fr> for SimpleCircuit {
+    fn synthesize<CS: ConstraintSystem<Fr>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
+    }
+}
