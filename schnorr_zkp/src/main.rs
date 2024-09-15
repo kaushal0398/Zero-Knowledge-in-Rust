cd6e81d1c4437db2d0985e4a1cd204cad565d8be
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-15 05:55:18 +0530

    initial commit

---
+++ schnorr_zkp/src/main.rs
@@ -0,0 +1,50 @@
+use curve25519_dalek::scalar::Scalar;
+use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+use rand::rngs::OsRng;
+use rand::RngCore;  // Import RngCore trait for generating random bytes
+
+fn schnorr_prove(secret: Scalar) -> (RistrettoPoint, Scalar, Scalar) {
+    let mut random_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut random_bytes);
+    let random_nonce = Scalar::from_bytes_mod_order(random_bytes);
+    
+    let commitment = random_nonce * RISTRETTO_BASEPOINT_POINT;
+    
+    let mut challenge_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut challenge_bytes);
+    let challenge = Scalar::from_bytes_mod_order(challenge_bytes);
+
+    let response = random_nonce + challenge * secret;
+    
+    (commitment, challenge, response)
+}
+
+
+fn schnorr_verify(public_key: RistrettoPoint, commitment: RistrettoPoint, challenge: Scalar, response: Scalar) -> bool {
+    let lhs = response * RISTRETTO_BASEPOINT_POINT;
+    
+    let rhs = commitment + challenge * public_key;
+    
+    lhs == rhs
+}
+
+fn main() {
+    let mut secret_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut secret_bytes);
+    let secret = Scalar::from_bytes_mod_order(secret_bytes);
+    
+    let public_key = secret * RISTRETTO_BASEPOINT_POINT;
+    
+    println!("Prover's Secret (hidden): {:?}", secret);
+    println!("Prover's Public Key: {:?}", public_key);
+    
+    let (commitment, challenge, response) = schnorr_prove(secret);
+    
+    println!("Commitment: {:?}", commitment);
+    println!("Challenge: {:?}", challenge);
+    println!("Response: {:?}", response);
+    
+    
+    println!("Is the proof valid? {}", is_valid);
+}
