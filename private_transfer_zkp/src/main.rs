dd3a813f5ac100ff120281dc1f13ac3cae642dc3
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-21 12:09:21 +0530

    initial commit

--- private_transfer_zkp/src/main.rs
+++ private_transfer_zkp/src/main.rs
@@ -20,3 +20,62 @@ struct PrivateTransfer {
     transfer_amount: u64,        
 }
 
+impl PrivateTransfer {
+    fn new(sender_balance: u64, receiver_balance: u64, transfer_amount: u64) -> Self {
+        let sender_blinding = Scalar::random(&mut thread_rng());
+        let receiver_blinding = Scalar::random(&mut thread_rng());
+        let transfer_blinding = Scalar::random(&mut thread_rng());
+
+        let sender_balance_commitment = pedersen_commit(sender_balance, sender_blinding);
+        let receiver_balance_commitment = pedersen_commit(receiver_balance, receiver_blinding);
+        let transfer_amount_commitment = pedersen_commit(transfer_amount, transfer_blinding);
+
+        Self {
+            sender_balance_commitment,
+            receiver_balance_commitment,
+            transfer_amount_commitment,
+            sender_blinding,
+            receiver_blinding,
+            transfer_blinding,
+            transfer_amount,
+        }
+    }
+}
+
+struct PrivateTransferCircuit {
+    sender_balance_commitment: RistrettoPoint,
+    receiver_balance_commitment: RistrettoPoint,
+    transfer_amount_commitment: RistrettoPoint,
+    transfer_amount: Option<Fr>,    
+    sender_blinding: Option<Fr>,    
+    receiver_blinding: Option<Fr>,  
+    transfer_blinding: Option<Fr>,  
+}
+
+impl Circuit<Fr> for PrivateTransferCircuit {
+    fn synthesize<CS: ConstraintSystem<Fr>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
+        let transfer_amount = AllocatedNum::alloc(cs.namespace(|| "transfer amount"), || {
+            self.transfer_amount.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+
+        let sender_blinding = AllocatedNum::alloc(cs.namespace(|| "sender blinding"), || {
+            self.sender_blinding.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        let receiver_blinding = AllocatedNum::alloc(cs.namespace(|| "receiver blinding"), || {
+            self.receiver_blinding.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+        let transfer_blinding = AllocatedNum::alloc(cs.namespace(|| "transfer blinding"), || {
+            self.transfer_blinding.ok_or(SynthesisError::AssignmentMissing)
+        })?;
+
+        cs.enforce(
+            || "verify transfer commitment",
+            |lc| lc + transfer_amount.get_variable() + transfer_blinding.get_variable(),
+            |lc| lc + CS::one(),
+            |lc| lc + CS::one(), 
+        );
+
+        Ok(())
+    }
+}
+
