c174e435d94808df63c53fa72710d0fed97fe17b
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-09-26 21:11:41 +0530

    initial commit

--- 5_schnorr_zkp/src/main.rs
+++ 5_schnorr_zkp/src/main.rs
@@ -38,8 +38,6 @@ fn main() {
     let (commitment, challenge, response) = schnorr_prove(secret);
     
     println!("Commitment: {:?}", commitment);
-    println!("Challenge: {:?}", challenge);
-    println!("Response: {:?}", response);
     
     let is_valid = schnorr_verify(public_key, commitment, challenge, response);
     
