f0a70085dff07eb254c99df99cda3f99ac2be3fc
kaushal panpaliya <kaushalpanpalia821@gmail.com>
2024-10-02 18:38:17 +0530

    updated

--- 9_zk_stark/src/main.rs
+++ 9_zk_stark/src/main.rs
@@ -4,7 +4,8 @@ use winterfell::{
 use winterfell::math::{fields::f128::BaseElement};
 use winterfell::prover::DefaultProver;
 use winterfell::verifier::verify;
-
+use winterfell::Air;
+use winterfell::air::AirConfig;
 
 struct FibonacciAir {
     sequence_length: usize,
