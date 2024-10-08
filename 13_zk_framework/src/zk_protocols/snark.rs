ba629aee989f679a2ed3b3e00af9de20d21827aa
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2024-10-08 08:30:28 +0530

    Update snark.rs

--- 13_zk_framework/src/zk_protocols/snark.rs
+++ 13_zk_framework/src/zk_protocols/snark.rs
@@ -1,4 +1,7 @@
 use bellman::groth16::{generate_random_parameters, create_random_proof, verify_proof, prepare_verifying_key};
+use pairing::bn256::{Bn256, Fr};
+use bellman::Circuit;
+use rand::thread_rng;
 
 pub fn create_snark_proof<C: Circuit<Fr>>(circuit: C) -> bool {
     
