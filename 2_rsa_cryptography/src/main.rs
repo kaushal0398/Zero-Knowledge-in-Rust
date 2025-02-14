25fa20f7fbc021cba0292f29b37dd1083a43ef02
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2025-02-14 23:28:14 +0530

    Update main.rs

--- 2_rsa_cryptography/src/main.rs
+++ 2_rsa_cryptography/src/main.rs
@@ -11,7 +11,6 @@ use rand::rngs::OsRng;
 
 fn generate_rsa_keypair() -> (RsaPrivateKey, RsaPublicKey) {
     let private_key = RsaPrivateKey::new(&mut OsRng, 2048).expect("Failed to generate a key");
-    let public_key = RsaPublicKey::from(&private_key);
     (private_key, public_key)
 }
 
